956af58205f87ed37751614845af54f4cac32ea6
Donea Fernando-Emanuel <fernandodonea10@gmail.com>
2025-05-20 01:03:41 +0300

    Add new data source configuration and create SQL sequences

---
+++ secvente.sql
@@ -0,0 +1,38 @@
+----crearea secventelor----
+
+--secventa utilizator--
+CREATE SEQUENCE seq_utilizator
+START WITH 1
+INCREMENT BY 1
+NOCACHE
+NOCYCLE;
+
+--secventa comenda--
+CREATE SEQUENCE seq_comanda
+START WITH 1000
+INCREMENT BY 1
+NOCACHE
+NOCYCLE;
+
+--secventa cd personalizat--
+CREATE SEQUENCE seq_cd_pers
+START WITH 1
+INCREMENT BY 1
+NOCACHE
+NOCYCLE;
+
+--secventa playlisy--
+CREATE SEQUENCE seq_playlist
+START WITH 1
+INCREMENT BY 1
+NOCACHE
+NOCYCLE;
+
+--secventa recenzie--
+CREATE SEQUENCE seq_recenzie
+START WITH 1
+INCREMENT BY 1
+NOCACHE
+NOCYCLE;
+
+
