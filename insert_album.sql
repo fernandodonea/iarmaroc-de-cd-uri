f0be2da8796224bdd41e06b7908cef5f5e50a0d4
Donea Fernando-Emanuel <fernandodonea10@gmail.com>
2025-05-20 19:42:30 +0300

    Add insert statements for ALBUM table with various albums and details

---
+++ insert_album.sql
@@ -0,0 +1,32 @@
+--inserari albume--
+
+INSERT INTO ALBUM (album_id, titlu, artist_id, data_lansare, pret, stoc, rating, descriere, numar_piese, format)
+VALUES(8, 'Hot Space', 1, TO_DATE('1982-05-21', 'YYYY-MM-DD'), 29.99, 40, 3.5, 'Album funk/pop', 11, 'CD');
+
+INSERT INTO ALBUM (album_id, titlu, artist_id, data_lansare, pret, stoc, rating, descriere, numar_piese, format)
+VALUES (1, 'To Pimp a Butterfly', 2, TO_DATE('2015-03-15', 'YYYY-MM-DD'), 49.99, 100, 4.8, 'Album hip-hop revolutionar',16, 'CD');
+
+INSERT INTO ALBUM (album_id, titlu, artist_id, data_lansare, pret, stoc, rating, descriere, numar_piese, format)
+VALUES (2, 'good kid, m.A.A.d city', 2, TO_DATE('2012-10-22', 'YYYY-MM-DD'), 44.99, 75, 4.7,'Album conceptual despre viata in Compton', 12, 'CD');
+
+INSERT INTO ALBUM (album_id, titlu, artist_id, data_lansare, pret, stoc, rating, descriere, numar_piese, format)
+VALUES (3, 'DAMN.', 2, TO_DATE('2017-04-14', 'YYYY-MM-DD'), 39.99, 150, 4.6, 'Album castigator premiul Pulitzer', 14, 'CD');
+
+INSERT INTO ALBUM (album_id, titlu, artist_id, data_lansare, pret, stoc, rating, descriere, numar_piese, format)
+VALUES (4, 'Folklore', 3, TO_DATE('2020-07-24', 'YYYY-MM-DD'), 34.99, 200, 4.5, 'Album indie folk', 16, 'DIGITAL');
+
+INSERT INTO ALBUM (album_id, titlu, artist_id, data_lansare, pret, stoc, rating, descriere, numar_piese, format)
+VALUES (5, 'Evermore', 3, TO_DATE('2020-12-11', 'YYYY-MM-DD'), 34.99, 180, 4.4, 'Album sora pentru Folklore', 15,'DIGITAL');
+
+INSERT INTO ALBUM (album_id, titlu, artist_id, data_lansare, pret, stoc, rating, descriere, numar_piese, format)
+VALUES(6, 'Random Access Memories', 4, TO_DATE('2013-05-17', 'YYYY-MM-DD'), 54.99, 90, 4.9, 'Album dance electronic', 13, 'CD');
+
+INSERT INTO ALBUM (album_id, titlu, artist_id, data_lansare, pret, stoc, rating, descriere, numar_piese, format)
+VALUES(7, 'Discovery', 4, TO_DATE('2001-03-12', 'YYYY-MM-DD'), 49.99, 60, 4.8, 'Album house futuristic', 14, 'CD');
+
+INSERT INTO ALBUM (album_id, titlu, artist_id, data_lansare, pret, stoc, rating, descriere, numar_piese, format)
+VALUES(9, 'Master of Puppets', 5, TO_DATE('1986-03-03', 'YYYY-MM-DD'), 44.99, 120, 5.0, 'Album thrash metal clasic', 8,'CD');
+
+INSERT INTO ALBUM (album_id, titlu, artist_id, data_lansare, pret, stoc, rating, descriere, numar_piese, format)
+VALUES (10, 'Ride the Lightning', 5, TO_DATE('1984-07-27', 'YYYY-MM-DD'), 44.99, 100, 4.9, 'Al doilea album Metallica',8, 'CD');
+
