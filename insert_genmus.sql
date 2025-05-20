61c866effde5a2c04917ef68c0d5b929047d848f
Donea Fernando-Emanuel <fernandodonea10@gmail.com>
2025-05-20 19:16:09 +0300

    Add insert statements for GEN_MUZICAL and ARTIST tables, and update SQL dialect mappings

---
+++ insert_genmus.sql
@@ -0,0 +1,15 @@
+-- Inserarea datelor în tabela GEN_MUZICAL
+INSERT INTO GEN_MUZICAL (gen_id, denumire, descriere)
+VALUES (seq_genmus.NEXTVAL, 'Rock', 'Gen muzical popular caracterizat prin utilizarea chitării electrice și a tobelor.');
+
+INSERT INTO GEN_MUZICAL (gen_id, denumire, descriere)
+VALUES (seq_genmus.NEXTVAL, 'Pop','Gen muzical comercial caracterizat prin melodii accesibile și structuri simple dar captivante.');
+
+INSERT INTO GEN_MUZICAL (gen_id, denumire, descriere)
+VALUES (seq_genmus.NEXTVAL, 'Hip-Hop','Gen muzical urban ce îmbină ritmuri puternice cu versuri rap și elemente de scratching.');
+
+INSERT INTO GEN_MUZICAL (gen_id, denumire, descriere)
+VALUES (seq_genmus.NEXTVAL, 'Electronic','Gen muzical bazat pe sunete produse electronic, sintetizatoare și beat-uri programate.');
+
+INSERT INTO GEN_MUZICAL (gen_id, denumire, descriere)
+VALUES (seq_genmus.NEXTVAL, 'Heavy-Metal','Gen muzical intens caracterizat prin chitare distorsionate, tobe puternice și vocale expresive.');
